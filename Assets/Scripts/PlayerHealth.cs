bc04352586d39bb7dbafeef8c83c6d8aad03f138
EthanF11 <ethan.flemming11@gmail.com>
2025-02-20 14:06:07 +0800

    Commit moment

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,26 +1,28 @@
-using UnityEngine;
-
-public class PlayerHealth : MonoBehaviour
+namespace Player
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class PlayerHealth : MonoBehaviour
     {
+        [SerializeField]
         int Current_Health = 100;
-     int Max_Health = 100;
-        range[0 - 100];
-     int Health_Potions = 5;
+        int Max_Health = 100;
+        int Health_Potions = 5;
         int Health_Potion_Heal = 20;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.H))
-            if (Current_Health = Max_Health)
-                Debug.Log("Your health is already at max!");
-        return;
-                 else (Current_Health = Current_Health + Health_Potion_Heal);
-                     Health_Potion_Heal = Health_Potion_Heal - 1;
-    Debug.Log("Health has increased by 20");
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.H))
+            {
+                if (Current_Health == Max_Health)
+                    Debug.Log("Your health is already at max!");
+                return;
+            }
+            else
+            {
+                (Current_Health =+ Health_Potions);
+                Health_Potion = Health_Potion - 1;
+                Debug.Log("Health has increased by 20");
+            }
+        }
     }
 }
