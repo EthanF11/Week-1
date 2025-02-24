928cca231927b704dc647b73742677210f900cfb
EthanF11 <ethan.flemming11@gmail.com>
2025-02-24 13:03:18 +0800

    Change to code

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,26 +2,25 @@ namespace Player
 {
     public class PlayerHealth : MonoBehaviour
     {
-        [SerializeField]
-        int Current_Health = 100;
-        int Max_Health = 100;
-        int Health_Potions = 5;
-        int Health_Potion_Heal = 20;
-
+       [SerializeField] int currentHealth = 100;
+       [SerializeField] int maxHealth = 100;
+       [SerializeField] int healthPotionHeal = 20;
+       [SerializeField] int healPotion = 5;
         // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.H))
             {
-                if (Current_Health == Max_Health)
+                if (currentHealth == maxHealth)
+                {
                     Debug.Log("Your health is already at max!");
-                return;
-            }
-            else
-            {
-                (Current_Health =+ Health_Potions);
-                Health_Potion = Health_Potion - 1;
-                Debug.Log("Health has increased by 20");
+                }
+                else
+                {
+                    currentHealth = currentHealth + healthPotionHeal;
+                    healthPotion--;
+                    Debug.Log("Health has increased by 20");
+                }
             }
         }
     }
